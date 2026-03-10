43d6566ad36338e93e20b3d08a5236087380bd93
jasmineliu0114 <jasmineliu0114@gmail.com>
2026-03-09 20:20:06 -0400

    hw 3

--- src/model.py
+++ src/model.py
@@ -6,24 +6,42 @@ def svm_classifier(kernel: str = "linear", C: float = 1.0, degree: int = 3, gamm
     """
     TODO: Return a scikit-learn SVC model with the specified parameters.
     """
-    pass
-
+    return SVC(kernel=kernel, C=C, degree=degree, gamma=gamma, random_state=42, probability=True)
 
 def svm_regressor(kernel: str = "linear", C: float = 1.0, degree: int = 3, gamma: str = "scale"):
     """
     TODO: Return a scikit-learn SVR model with the specified parameters.
     """
-    pass
+    return SVR(kernel=kernel, C=C, degree=degree, gamma=gamma)
 
 def evaluate_classifier(model, X_test, y_test):
     """
     TODO: Compute and return accuracy, precision, recall, and F1 score
     """
-    pass
-
+    n_classes = len(np.unique(y_test))
+    avg = "macro" if n_classes > 2 else "binary"
+    y_pred = model.predict(X_test)
+    
+    # Evaluation scores
+    metrics = {
+        "accuracy": accuracy_score(y_test, y_pred),
+        "precision": precision_score(y_test, y_pred, average=avg, zero_division=0),
+        "recall": recall_score(y_test, y_pred, average=avg, zero_division=0),
+        "f1": f1_score(y_test, y_pred, average=avg, zero_division=0)
+    }
+    return metrics
+    
 
 def evaluate_regressor(model, X_test, y_test):
     """
     TODO: Compute and return MAE, RMSE, and R2
     """
-    pass
+    y_pred = model.predict(X_test)
+    
+    # Evaluation scores
+    metrics = {
+        'mae': mean_absolute_error(y_test, y_pred),
+        'rmse': np.sqrt(mean_squared_error(y_test, y_pred)),
+        'r2': r2_score(y_test, y_pred)
+    }
+    return metrics
